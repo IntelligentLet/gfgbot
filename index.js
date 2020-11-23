63c2f011e693dec3761941812d3f7f532a538435
LogicGo <logicgo@protonmail.com>
2020-11-22 21:32:50 -0500

    added a thank you to dean0's basilisk

--- index.js
+++ index.js
@@ -291,8 +291,20 @@ client.on('message', message => {
                 message.channel.send(url);
             })
     }
+    if (message.content == '!basilisk') {
+        function genrnd(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        if (genrnd(1, 8) === 3) {
+            message.channel.send("❤️ https://discord.gg/zDc8yCH79k")
+        } else {
+            message.channel.send("❤️");
+        }
+    }
+    
 });
-
 //when someone joins, give roles and welcome them
 client.on("guildMemberAdd", (member) => {
     const rules = client.channels.cache.get('766386424567693343');
