a68425700caa33035748d6b251922b0bbd1c4676
LogicGo <logicgo@protonmail.com>
2021-01-09 23:53:37 -0500

    bugfix: reason malfunctions on ban and mute

--- commands/ban.js
+++ commands/ban.js
@@ -7,9 +7,10 @@ module.exports = {
         var victim = message.member.guild.member(message.mentions.users.first());
         if (message.member.roles.cache.some(role => role.name === 'Mod')) {
             if (message.member.guild.member(victim) && victim.roles.cache.some(role => role.name === 'Mod') === false) {
-                var reason = message.content.toString().substring((args[0].length + args[1].length), message.content.toString().length);
+                args.shift();
+                var reason = args.join(' ');
                 message.member.guild.members.ban(victim);
-                message.channel.send(`Banned ${victim} for reason ${reason.substring(2, reason.length)}`);
+                message.channel.send(`Banned ${victim} for reason ${reason}`);
             } else {
                 message.channel.send(`Eeek! Something went wrong!`);
             }
--- commands/mute.js
+++ commands/mute.js
@@ -6,10 +6,11 @@ module.exports = {
         var victim = message.member.guild.member(message.mentions.users.first());
         if (message.member.roles.cache.some(role => role.name === 'Mod')) {
             if (message.member.guild.member(victim) && victim.roles.cache.some(role => role.name === 'Mod') === false) {
-                var reason = message.content.toString().substring((args[0].length + args[1].length), message.content.toString().length);
+                args.shift();
+                var reason = args.join(' ');
                 var muterole = message.member.guild.roles.cache.find(role => role.name === 'Muted');
                 victim.roles.add(muterole).catch(console.error);
-                message.channel.send(`Muted ${victim} for reason ${reason.substring(2, reason.length)}`);
+                message.channel.send(`Muted ${victim} for reason ${reason}`);
             } else {
                 message.channel.send(`Eeek! Something went wrong!`);
             }
